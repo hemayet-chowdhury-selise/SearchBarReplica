a7a73c0fe584b3c28880961fc0364dce652e0027
hemayet-chowdhury-selise <hemayet.chowdhury@selise.ch>
2020-10-18 05:55:32 -0700

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EasyBarComponent } from './easy-bar/easy-bar.component';
+import { CustomBarComponent } from './custom-bar/custom-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EasyBarComponent,
+    CustomBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/custom-bar/custom-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-bar',
+  templateUrl: './custom-bar.component.html',
+  styleUrls: ['./custom-bar.component.scss']
+})
+export class CustomBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
