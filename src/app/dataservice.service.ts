2f4403a6fe3fef148417e4f192648dac8c995424
hemayet-chowdhury-selise <hemayet.chowdhury@selise.ch>
2020-10-21 22:32:48 +0600

    border radius fixed

--- src/app/dataservice.service.ts
+++ src/app/dataservice.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class DataserviceService {
 
   searchList = [
-    'Angular', 'React', 'Vue', 'Node', 'Angular Material', 'Semantic HTML', 'Hyper Text Mark-up Language', 'Hemayet', 'Abdun Nahid', 'Asif Adham'
+    'angular', 'react', 'vue', 'node', 'angular material', 'semantic html', 'hyper text mark-up language'
   ];
 
   constructor() { }
