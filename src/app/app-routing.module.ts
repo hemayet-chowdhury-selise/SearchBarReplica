19c1f2a08b5e71aa0f7f2bc619083d2fda877cda
hemayet-chowdhury-selise <hemayet.chowdhury@selise.ch>
2020-10-29 15:33:36 +0600

    add query params on selection of item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomBarComponent } from './custom-bar/custom-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:  CustomBarComponent},
+{ path: 'results', component: CustomBarComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
