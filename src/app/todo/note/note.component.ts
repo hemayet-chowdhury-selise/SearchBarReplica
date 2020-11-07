1b27365d3867d90a0c4364a2326554086195ced5
hemayet-chowdhury-selise <hemayet.chowdhury@selise.ch>
2020-11-08 02:42:37 +0600

    Added search bar functionalities. cleaned up UI

--- src/app/todo/note/note.component.ts
+++ src/app/todo/note/note.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+
+import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { PostService } from '../shared/post.service';
 
 @Component({
   selector: 'app-note',
@@ -9,11 +12,67 @@ export class NoteComponent implements OnInit {
 
   @Input('title') title: String;
   @Input('note') note: String;
+  @Input('postId') id: number;
+  @ViewChild('optionsDiv') optionsDiv: ElementRef;
+  @ViewChild('more') more: ElementRef;
   showIcons:boolean =  false;
+  showModal:boolean = false;
 
-  constructor() { }
+  constructor(private postService: PostService, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+
+  onToggleIcons(){
+    if(this.showModal==true){
+      this.showIcons = true;
+    }
+    else this.showIcons = !this.showIcons;
+  }
+
+  onOtherIconClick(){
+    event.stopPropagation();
+  }
+
+  onOptionsClick(){
+    event.stopPropagation();
+
+    this.showModal = !this.showModal;
+    return;
+
+
+  }
+
+  onOptionsSelected(){
+    event.stopPropagation();
+
+    this.router.navigate(['/note/'+this.id+'/edit']);
+  }
+
+  onItemDelete(){
+    event.stopPropagation();
+
+
+    this.postService.deletePost(this.id);
+  }
+
+  @HostListener('document:mousedown', ['$event'])
+  onGlobalClick(event): void {
+
+    if(this.optionsDiv==undefined){
+
+    }
+    else if (this.optionsDiv.nativeElement.contains(event.target) ) {
+      // clicked outside => close dropdown list
+
+   }
+   else{
+    this.showModal = false;
+    }
+
+
+  }
+
 
 }
